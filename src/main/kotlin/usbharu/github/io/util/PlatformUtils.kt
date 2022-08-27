368f41d11223d6194fda6f0fefdaa92afc5b0493
haru <harujisaku@gmail.com>
2022-08-27 23:15:14 +0900

    PlatformUtilsのドキュメントを追加

--- src/main/kotlin/usbharu/github/io/util/PlatformUtils.kt
+++ src/main/kotlin/usbharu/github/io/util/PlatformUtils.kt
@@ -1,14 +1,23 @@
 package usbharu.github.io.util
 
 import java.util.*
-
+/**
+プラットフォームに関するユーティリティークラス
+*/
 object PlatformUtils {
     val OS_NAME = System.getProperty("os.name").lowercase(Locale.getDefault())
+
+    /**
+        現在のプラットフォームがlinuxならtrueを返す。
+    */
 
     fun isLinux():Boolean{
         return OS_NAME.startsWith("linux")
     }
 
+    /**
+        現在のプラットフォームがwindowsならtrueを返す。
+    */
     fun isWindows():Boolean{
         return OS_NAME.startsWith("windows")
     }
